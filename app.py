66a86fb84d8c0ee84593b47c061c81b8c3ab5b98
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-08-23 19:25:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+import privateGPT
+import traceback
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        message = request.form['message']
+        response = privateGPT.run_model(message)
+        return jsonify(response=response)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+        print("Traceback:", traceback.format_exc())
+        return jsonify(error=str(e)), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
